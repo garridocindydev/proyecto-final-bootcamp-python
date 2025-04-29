50f28d8f6beb5b509fc541cc4e46133614945afc
Cindy Garrido S <cindygarridosuarez@gmail.com>
2025-04-28 23:14:33 -0500

    Revert "primer commit v0.1"
    This reverts commit d12ad90c3a16c4bc9a67ba354d84fc3258adb072.

--- flask_app/controllers/usuario.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, request ,redirect,session
-from flask_app import app,bcrypt  # Importamos la app de la carpeta flask_app
-
-
-
-@app.route("/", methods=["GET"])
-def index():
-   return render_template("index.html")
